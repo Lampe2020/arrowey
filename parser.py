86d5afa69927cfa603e3c36eeb9e816b85f00136
Lampe2020 <Stehlampe2020@gmail.com>
2024-01-15 15:23:44 +0100

    Update parser.py * Rename function `printlog` to `logmsg` * Move testing code into `if __name__ == '__main__':` block

--- parser.py
+++ parser.py
@@ -5,14 +5,14 @@ arr_version = '0.1.0'
 with open('arrowey.lark','r') as larkfile:
     parser = lark.Lark(larkfile.read())
 
-def printlog(mood:int=0,msg:str|tuple[str,...]|list[str]|set[str]='')->None:
+def logmsg(mood:int=0,msg:str|tuple[str,...]|list[str]|set[str]='')->None:
     """
     Print one or more log message(s) with associated status.
     """
     if type(msg)==str:
         msg = (msg,)
     elif type(msg) not in (tuple,list,set):
-        printlog(-1, (f'printlog(): message list has wrong type: {repr(type(msg).__name__)}',))
+        logmsg(-1, (f'logmsg(): message list has wrong type: {repr(type(msg).__name__)}',))
         msg = (repr(msg),)
     match mood:
         # msym: Mood SYMbol
@@ -31,28 +31,29 @@ def printlog(mood:int=0,msg:str|tuple[str,...]|list[str]|set[str]='')->None:
     for message in msg:
         print(f'[{msym}] {message}')
 
-printlog(0, (
+if __name__ == '__main__':
+    logmsg(0, (
         f'arrowey v{arr_version}',
         'Type "end:0" (without the double quotes) or press [Ctrl]+[D] to exit.'
     ))
-while True:
-    try:
-        inp = input('arrowey>>> ')
-    except EOFError:
-        inp = 'end:0'
-    except Exception as err:
-        from traceback import format_exception
-        nl:str='\n'
-        printlog(-3, f'Error while reading stdin!\n{nl.join(format_exception(err))}')
-        inp = 'end:0'
-    if inp == 'end:0':
-        printlog(0,'arrowey stopped')
-        break
-    if inp:
+    while True:
         try:
-            AST = parser.parse(inp)
+            inp = input('arrowey>>> ')
+        except EOFError:
+            inp = 'end:0'
         except Exception as err:
-            AST = None
-            printlog(-2,f'Parser error!\n→ {type(err).__name__}: {err}')
-            continue
-        printlog(1,f'AST successfully generated:\n{AST.pretty()}')
+            from traceback import format_exception
+            nl:str='\n'
+            logmsg(-3, f'Error while reading stdin!\n{nl.join(format_exception(err))}')
+            inp = 'end:0'
+        if inp == 'end:0':
+            logmsg(0, 'arrowey stopped')
+            break
+        if inp:
+            try:
+                AST = parser.parse(inp)
+            except Exception as err:
+                AST = None
+                logmsg(-2, f'Parser error!\n→ {type(err).__name__}: {err}')
+                continue
+            logmsg(1, f'AST successfully generated:\n{AST.pretty()}')
