57e6fc7a1f8419218000fa905de1ccc6fdd56386
Lampe2020 <kontakt@lampe2020.de>
2024-05-18 17:27:45 +0200

    Delete recursive_parser.py

--- recursive_parser.py
+++
@@ -1,5 +0,0 @@
-def recursive_parse(code:str)->any:
-    """
-    Same as parse(), just using Python's stack to make our lives easier.
-    """
-    sys.setrecursionlimit(3_333) # Some high number to prevent arrowey from crashing too easily with recursion errors
